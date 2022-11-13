2bc1d0edf18c33faf2476aa04720452c243f64f2
XimenaMNgrn <118090746+XimenaMNgrn@users.noreply.github.com>
2022-11-13 15:52:16 -0600

    Cambios en la conexión del socket

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var io = require('socket.io')(server);
 app.use(express.static('public'));
 server.listen(PORT, () => console.log('Servidor iniciado en puerto '+PORT));
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+app.get('', function (req, res) {
+  res.sendFile(__dirname + 'public/index.html');
 });
 
 io.on('connection', function (socket) {
