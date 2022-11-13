bb8b02f9089beef5f06f551a39868aba0f7ee75c
XimenaMNgrn <118090746+XimenaMNgrn@users.noreply.github.com>
2022-11-13 15:16:11 -0600

    Cambio en puerto e ip

--- public/js/logica.js
+++ public/js/logica.js
@@ -1,6 +1,6 @@
 const PORT = process.env.PORT || 8000;
 // https://chatnode-ximena.herokuapp.com/
-var socket = io.connect(`http://208.39.138.12:{PORT}`);
+var socket = io.connect(`http://201.141.117.39:{PORT}`);
 
 var list = document.querySelector(`#not`)
 let mensaje = document.getElementById('mensaje');
